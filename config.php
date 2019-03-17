a9a96e6c0100bf9ebcd9b16ab6722b2c43098c34
Sylvain Julé <sylvain.jule@free.fr>
2019-03-17 17:19:34 +0100

    Follow the new Pluginkit guidelines

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Kirby::plugin('sylvainjule/autofocus', array(
-	'api'    => require_once __DIR__ . '/lib/api.php',
-));
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+Kirby::plugin('sylvainjule/autofocus', array(
+	'api'    => require_once __DIR__ . '/lib/api.php',
+));
